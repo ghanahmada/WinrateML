6041f4913b2902a2e5a8ebdddcaa66b24635cd26
Ghana Ahmada Yudistira <103763579+ghanahmada@users.noreply.github.com>
2022-07-01 13:49:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,36 +8,23 @@ wr1 = st.number_input("Ketik winrate Anda",0.0)
 wr2 = st.number_input("Ketik winrate yang Anda inginkan",0.0)
 hitung = st.button("Hitung Winrate")
 
-def calc(match1=match1, wr1=wr1, wr2=wr2):
+def calc_match():
     match2Win = ceil((match1 * (wr2 - wr1)) / (100 - wr2))
-    match2Lose = floor((match1 * (wr2 - wr1)) / wr2)
+    match2Lose = abs(floor((match1 * (wr2 - wr1)) / wr2))
     return (match2Win,"winstreak") if wr2 > wr1 else (match2Lose,"losestreak")
 
 if hitung:
     try:
-        st.markdown(f"Anda harus bermain **{calc()[1]}** sebanyak **{abs(calc()[0])}** kali")
-        details = st.expander("Perkembangan Winrate")
-        try:
-            if calc()[1] == "winstreak":
-                for match_Ke in range(1, ceil(calc()[0]) + 1, 1):
-                    wr2 = ((100 * match_Ke) + (match1 * wr1)) / (match1 + match_Ke)
-                    if ceil(calc()[0]) > 100 and match_Ke == 51:
-                        details.write("...")
-                        for match_Ke in range(ceil(calc()[0])-49,ceil(calc()[0]) + 1, 1):
-                            wr2 = ((100 * match_Ke) + (match1 * wr1)) / (match1 + match_Ke)
-                            details.write(f"match ke-{str(match_Ke)} = {str(round(wr2, 2))}%")
-                        break
-                    details.write(f"match ke-{str(match_Ke)} = {str(round(wr2, 2))}%")
-            else:
-                for match_Ke in range(1, abs(floor(calc()[0])) + 1, 1):
-                    wr2 = (match_Ke + (match1 * wr1)) / (match1 + match_Ke)
-                    if abs(floor(calc()[0])) > 100 and match_Ke == 51:
-                        details.write("...")
-                        for match_Ke in range(abs(floor(calc()[0]))-49,abs(floor(calc()[0])) + 1, 1):
-                            wr2 = (match_Ke + (match1 * wr1)) / (match1 + match_Ke)
-                            details.write(f"match ke-{str(match_Ke)} = {str(round(wr2, 2))}%")
-                        break
-                    details.write(f"match ke-{str(match_Ke)} = {str(round(wr2, 2))}%")
-        except:
-            details.markdown("*None*")
-    except: st.error("Mohon isi kolom di atas")
+        st.markdown(f"Anda harus bermain **{calc_match()[1]}** sebanyak **{calc_match()[0]}** kali")
+        details,match_ke = st.expander("Perkembangan Winrate"), 1
+        while match_ke <= calc_match()[0]:
+            def calc_match():
+                wr2_win = ((100 * match_ke) + (match1 * wr1)) / (match1 + match_ke)
+                wr2_lose = (match_ke + (match1 * wr1)) / (match1 + match_ke)
+                return wr2_win if calc_match()[1] == "winstreak" else wr2_lose
+            if calc_match()[0] > 100 and match_ke == 51:
+                details.markdown("...")
+                match_ke = calc_match()[0]-49
+            details.markdown(f"match ke-{match_ke} = {round(calc_match(), 2)}%")
+            match_ke += 1
+    except: st.error("Mohon isi kolom di atas.")
