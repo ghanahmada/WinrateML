f3b444cf61846c7c03057c3f394d39c74ed97581
Ghana Ahmada Yudistira <103763579+ghanahmada@users.noreply.github.com>
2022-07-01 11:27:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,18 @@ def calc(match1=match1, wr1=wr1, wr2=wr2):
     return (match2Win,"winstreak") if wr2 > wr1 else (match2Lose,"losestreak")
 
 if hitung:
-    st.markdown(f"Anda harus bermain {calc()[1]} sebanyak **{calc()[0]}** kali")
+    try:
+        st.markdown(f"Anda harus bermain **{calc()[1]}** sebanyak **{abs(calc()[0])}** kali")
+        details = st.expander("Perkembangan Winrate")
+        try:
+            if calc()[1] == "winstreak":
+                for match_Ke in range(1, ceil(calc()[0]) + 1, 1):
+                    wr2 = ((100 * match_Ke) + (match1 * wr1)) / (match1 + match_Ke)
+                    details.write(f"match ke-{str(match_Ke)} = {str(round(wr2, 2))}%")
+            else:
+                for match_Ke in range(1, abs(floor(calc()[0])) + 1, 1):
+                    wr2 = (match_Ke + (match1 * wr1)) / (match1 + match_Ke)
+                    details.write(f"match ke-{str(match_Ke)} = {str(round(wr2, 2))}%")
+        except:
+            details.markdown("*None*")
+    except: st.error("Mohon isi kolom di atas")
